c782f1ce5b43a5f054b777a0d77d410ccb30bf45
ttbug <ttbuge@gmail.com>
2017-04-23 22:25:11 +0800

    添加文章分页功能

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -2,13 +2,16 @@ import markdown
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Tag
 from django.conf import settings
+from utils.paginator import paginate_queryset
 # Create your views here.
 
 
 def index(request):
-    post_list = Post.objects.all()
+    post_list = Post.objects.all().order_by("-id")
+    page_no = int(request.GET.get("page_no", "1"))
+    page_articles, pagination_data = paginate_queryset(post_list, page_no)
     return render(request, 'blog/index.html', context={
-            'post_list': post_list}
+        'post_list': page_articles, 'pagination_data': pagination_data}
     )
 
 
@@ -33,14 +36,20 @@ def archives(request, year, month):
 # 处理文章标签
 def category(request, pk):
     cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate)
-    return render(request, "blog/index.html", context={"post_list": post_list})
+    post_list = Post.objects.filter(category=cate).order_by("id")
+    page_no = int(request.GET.get("page_no", "1"))
+    page_articles, pagination_data = paginate_queryset(post_list, page_no)
+    return render(request, "blog/index.html", context={"post_list": page_articles,
+        "pagination_data": pagination_data})
 
 
 def tags(request, pk):
     label = get_object_or_404(Tag, pk=pk)
-    post_list = Post.objects.filter(tags=label)[::-1]
-    return render(request, "blog/index.html", context={"post_list": post_list})
+    post_list = Post.objects.filter(tags=label).order_by("-id")
+    page_no = int(request.GET.get("page_no", "1"))
+    page_articles, pagination_data = paginate_queryset(post_list, page_no)
+    return render(request, "blog/index.html", context={"post_list": page_articles,
+        "pagination_data": pagination_data})
 
 
 def global_settings(request):
