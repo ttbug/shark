82c8e39b2373ddfb258cb3226ef08a20a31ad359
ttbug <ttbuge@gmail.com>
2017-05-06 22:39:01 +0800

    添加代码高亮

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,4 +1,3 @@
-import markdown
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category, Tag
 from django.conf import settings
@@ -17,13 +16,6 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body,
-            extensions=[
-                'markdown.extensions.extra',
-                'markdown.extensions.codehilite',
-                'markdown.extensions.toc'
-            ]
-            )
     return render(request, 'blog/detail.html', context={'post': post})
 
 
