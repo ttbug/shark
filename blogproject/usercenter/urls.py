e82b3f988e6a3f714c8bf2ed8140d7aa6e9fefb3
ttbug <ttbuge@gmail.com>
2017-05-16 21:38:11 +0800

    完善重置密码功能，添加找回密码功能

--- blogproject/usercenter/urls.py
+++ blogproject/usercenter/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 
-app_name = "usercenter"
+# app_name = "usercenter"
 urlpatterns = [
     url(r'^register$', views.register, name="register"),
     url(r'^activate/(?P<code>\w+)', views.activate, name="activate"),
